bf132317fea617e92c0ca30be1feba02a3042e63
rjaramilloislas <49929855+rjaramilloislas@users.noreply.github.com>
2019-04-23 21:36:24 -0500

    Correccion para almacenar passwords en md5
    La clase tenia un error para poder regresar el valor del hash correcto, el problema fue resuelto.

--- web/app/helpers/HashUtils.java
+++ web/app/helpers/HashUtils.java
@@ -7,17 +7,18 @@ import java.security.MessageDigest;
 public class HashUtils {
 
     public static String getMd5(String s){
+    	String hashtext="";
         try {
             MessageDigest m = MessageDigest.getInstance("MD5");
             m.reset();
             m.update(s.getBytes());
             byte[] digest = m.digest();
             BigInteger bigInt = new BigInteger(1,digest);
-            String hashtext = bigInt.toString(16);
+            hashtext = bigInt.toString(16);
             while(hashtext.length() < 32){
                 hashtext = "0" + hashtext;
             }
         } catch (Exception e) {}
-        return s;
+        return hashtext;
     }
 }
